6ce9c873f84d204ed701e91f8dffbabe615f4cdf
jarett <afrocious@gmail.com>
2014-02-27 02:50:19 -0500

    set up arrays

--- Projectfiles/HelloWorldLayer.m
+++ Projectfiles/HelloWorldLayer.m
@@ -16,15 +16,36 @@
 {
 	if ((self = [super init]))
 	{
-		glClearColor(0.1f, 0.1f, 0.3f, 1.0f);
-
-		// "empty" as in "minimal code & resources"
-		CCLabelTTF* label = [CCLabelTTF labelWithString:@"Minimal Kobold2D Project"
-											   fontName:@"Arial"
-											   fontSize:20];
-		label.position = [CCDirector sharedDirector].screenCenter;
-		label.color = ccCYAN;
-		[self addChild:label];
+		//create a 2d array with given dimensions
+        NSMutableArray* grid = [[NSMutableArray alloc] init];
+        
+        for(int row = 0; row < NUM_ROWS; row ++)
+        {
+            NSMutableArray* gridSubArr = [[NSMutableArray alloc] init];
+            for(int col = 0; col < NUM_COLUMNS; col ++)
+            {
+                NSNumber *gridItem  = @0; //0 = dead, 1 = alive
+                
+                [gridSubArr addObject:gridItem];
+                
+            }
+            [grid addObject:gridSubArr];
+        }
+        
+        //create a 2d array to store the number of neighbors each cell has
+        NSMutableArray *numNeighbors = [[NSMutableArray alloc] init];
+        
+        for(int row = 0; row < NUM_ROWS; row ++)
+        {
+            NSMutableArray* numSubArr = [[NSMutableArray alloc] init];
+            for(int col = 0; col < NUM_COLUMNS; col ++)
+            {
+                NSNumber *numItem = @0;
+                
+                [numSubArr addObject:numItem];  //0 = dead, 1 = alive
+            }
+            [numNeighbors addObject:numSubArr]
+        }
 	}
 
 	return self;
